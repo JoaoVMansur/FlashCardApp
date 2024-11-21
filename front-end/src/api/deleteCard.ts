583a43899de00800436b3af3210fb4d28bfaf4f1
JoaoVMansur <jvictormansur@gmail.com>
2024-11-20 23:23:41 -0300

    teste

--- front-end/src/api/deleteCard.ts
+++ front-end/src/api/deleteCard.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const deleteCard = async (cardId: number) => {
   const response = await axios.delete(`http://localhost:8080/card/${cardId}`, {withCredentials:true});
+  console.log(response.data);
   return response.data;
 };
 
